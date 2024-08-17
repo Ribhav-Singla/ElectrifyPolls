3dcc13e986f1b9fa632393496eeca153ec8b89d4
ribhavsingla <ribhavsingla2166@gmail.com>
2024-08-17 08:44:18 +0530

    checking_ip_issue_1

--- backend/middleware.js
+++ backend/middleware.js
@@ -6,6 +6,8 @@ const ipAddress_inserter = async (roomId, ipAddress) => {
     if(!pollData){
         const poll = new Poll({ roomId, ipAddress , vote:false});
         await poll.save();
+        console.log('poll: ',poll);
+        
     }
   } catch (error) {
     console.log("error occured while inserting the ipAddress: ", error);
@@ -41,6 +43,7 @@ const ipAddress_updater = async (roomId, ipAddress) => {
         },
       }
     );
+    console.log('vote_details updated: ',vote_details);
     await vote_details.save();
   } catch (error) {
     console.log("error occured while ipAddress_updater: ", error);
